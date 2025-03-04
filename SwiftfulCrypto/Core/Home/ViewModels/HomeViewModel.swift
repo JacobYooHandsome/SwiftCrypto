0b3d1501618d02f93afa967f90c0a5d9f37e6af6
jacobyoohandsome <tolearneverything123@gmail.com>
2025-03-04 16:10:19 -0500

    Connect CoinDataService to HomeViewModel

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 class HomeViewModel: ObservableObject {
     
@@ -13,16 +14,18 @@ class HomeViewModel: ObservableObject {
     @Published var portfolioCoins: [CoinModel] = []
     
     private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
     
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(MockCoinData.coin)
-            self.portfolioCoins.append(MockCoinData.coin)
-        }
+        addSubscribers()
     }
     
     func addSubscribers() {
-        data
+        dataService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
     
 }
