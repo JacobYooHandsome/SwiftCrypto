799b6e6150e7d3454cb6893342302f43e133a206
jacobyoohandsome <tolearneverything123@gmail.com>
2025-04-01 20:15:24 -0400

    Add CoinImage functionality and Dev Preview

---
+++ SwiftfulCrypto/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,37 @@
+//
+//  CoinImageView.swift
+//  SwiftfulCrypto
+//
+//  Created by Jacob Yoo on 4/1/25.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundStyle(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+#Preview {
+    CoinImageView(coin: DeveloperPreview.coin)
+        .padding()
+}
